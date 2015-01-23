455dfae66cffad75d292d36bbbc6ec2340be594d
Winston Riley <masonwr@mac.com>
2015-01-23 11:45:08 -0700

    cleans up constructor stuff

--- src/gui/views/GUIRegion.java
+++ src/gui/views/GUIRegion.java
@@ -16,12 +16,14 @@ public class GUIRegion
   private Region region;
   private MapConverter converter; //set at class def.
   private Area area;
-  private RegionView look = new PassiveRegion(); // as default value
+  private RegionView look;
   private Polygon poly;
 
-  public GUIRegion(Region region)
+  public GUIRegion(Region region, MapConverter converter, RegionView look)
   {
     this.region = region;
+    this.converter = converter;
+    this.look = look;
   }
 
   public Polygon getPoly()
