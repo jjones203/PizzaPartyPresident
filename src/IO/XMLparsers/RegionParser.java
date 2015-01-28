107599a213c965638a209705a4583379b2dc70dd
Winston Riley <masonwr@mac.com>
2015-01-27 17:58:06 -0700

    adds parser for statxml

---
+++ src/IO/XMLparsers/RegionParser.java
@@ -0,0 +1,18 @@
+package IO.XMLparsers;
+
+import model.Region;
+import org.xml.sax.Locator;
+
+import java.util.List;
+
+/**
+ * Created by winston on 1/27/15.
+ * Phase_01
+ * CS 351 spring 2015
+ */
+public interface RegionParser
+{
+  List<Region> getRegionList();
+
+  Locator getLocator();
+}
---
+++ src/IO/XMLparsers/StateParserTest.java
@@ -0,0 +1,51 @@
+package IO.XMLparsers;
+
+import IO.AreaXMLloader;
+import model.MapPoint;
+import model.Region;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+public class StateParserTest
+{
+  public static void main(String[] args)
+  {
+    Collection<Region> regions = null;
+    try
+    {
+      AreaXMLloader loader = new AreaXMLloader(new StateParser(), "seom");
+      regions = loader.parseFile("resources/states.xml");
+    }
+    catch (ParserConfigurationException e)
+    {
+      e.printStackTrace();
+    }
+    catch (SAXException e)
+    {
+      e.printStackTrace();
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+
+    for (Region region : regions)
+    {
+      System.out.println(region);
+      for (MapPoint mp : region.getPerimeter())
+      {
+        System.out.print("\t" + mp);
+      }
+      System.out.println();
+    }
+
+    System.out.println("all done!");
+  }
+
+}
