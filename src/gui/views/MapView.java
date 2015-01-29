b59e186680f04b48230b6fd617b90b7bbb7c186d
Winston Riley <masonwr@mac.com>
2015-01-29 11:52:19 -0700

    better version of click at

--- src/gui/views/MapView.java
+++ src/gui/views/MapView.java
@@ -69,7 +69,10 @@ public class MapView
   {
     for (GUIRegion guir : getGuiRegions())
     {
-      if (guir.getPoly().contains(x, y)) guir.setActive(true);
+      if (guir.getPoly().contains(x, y))
+      {
+        guir.setActive(! guir.isActive());
+      }
     }
   }
 
@@ -85,6 +88,7 @@ public class MapView
 
   public Collection<GUIRegion> getRegionsInview(Camera camera)
   {
+    
 //    Collection<GUIRegion> regionsInview = new LinkedList<>();
 
 //    Rectangle2D inViewBox = camera.getViewBounds();
