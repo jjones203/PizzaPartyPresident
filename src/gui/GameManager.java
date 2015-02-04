0f24da492b3ffa8d9c5cd3e83318b40a7eb186de
David Ringo <davidmringo@gmail.com>
2015-02-04 13:02:43 -0700

    start building MapPane (wraps camera, controls)

---
+++ src/gui/GameManager.java
@@ -0,0 +1,33 @@
+package gui;
+
+import gui.hud.LowerPanel;
+import model.World;
+
+import javax.swing.*;
+
+/**
+ @author david
+ created: 2015-02-03
+
+ description:
+ 
+ */
+public class GameManager
+{
+ 
+ private MapPane mapPane;
+ private LowerPanel infoPane;
+ private World world;
+ 
+ public GameManager(World world, WorldPresenter presenter)
+ {
+
+  
+ }
+ 
+ 
+ public void newGame(World world)
+ {
+  
+ }
+}
--- src/gui/MapPane.java
+++ src/gui/MapPane.java
@@ -1,49 +1,71 @@
 package gui;
 
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
 
 /**
- * @author david
- *         created: 2015-01-27
- *         <p/>
- *         description:
- */
-public class CamController 
-        extends KeyAdapter 
-        implements ActionListener, MouseListener, MouseWheelListener
+ @author david
+ created: 2015-02-03
+
+ description: */
+
+
+public class MapPane extends JPanel
+  implements MouseListener, MouseWheelListener, MouseInputListener, KeyListener
 {
+
   private final static int CAMERA_STEP = 10;
   private final static double ZOOM_STEP = .05;
   private final static int
-  UP = 38,
-  LEFT = 37,
-          DOWN = 40,
-          RIGHT = 39,
-          SHIFT = 16;
-
+    UP = 38,
+    LEFT = 37,
+    DOWN = 40,
+    RIGHT = 39,
+    SHIFT = 16;
+  
   private boolean
-          isUPdepressed,
-          isDOWNdepressed,
-          isLEFTdepressed,
-          isRIGHTdepressed,
-          isSHIFTdepressed;
+    isUPdepressed,
+    isDOWNdepressed,
+    isLEFTdepressed,
+    isRIGHTdepressed,
+    isSHIFTdepressed;
 
+
+  private boolean isInDrag;
+  private Point2D draggedFrom;
+
+  
+  private WorldPresenter presenter;
   private Camera cam;
-  private WorldPresenter worldPresenter;
+  
 
-
-  public CamController(Camera camera, WorldPresenter worldPresenter)
+  public MapPane(Camera cam, WorldPresenter presenter)
   {
-    this.cam = camera;
-    this.worldPresenter = worldPresenter;
+    this.cam = cam;
+    this.presenter = presenter;
+    addMouseListener(this);
+    addMouseListener(this);
+    addMouseMotionListener(this);
+    addKeyListener(this);
+  }
+  
+
+  @Override
+  protected void paintComponent(Graphics g)
+  {
+    Graphics2D g2 = (Graphics2D) g;
+    g2.setTransform(cam.getTransform());
+
+    for (GUIRegion region : presenter.getRegionsInview(cam)) region.draw(g2);
   }
 
 
-  @Override
-  public void actionPerformed(ActionEvent e)
+  public void update()
   {
 
     if (isDOWNdepressed && isSHIFTdepressed)
@@ -56,9 +78,9 @@ public class CamController
       cam.zoomIn(ZOOM_STEP);
       return;
     }
-    if (isDOWNdepressed)  cam.translateRelativeToView(0, CAMERA_STEP);
-    if (isUPdepressed)    cam.translateRelativeToView(0, -CAMERA_STEP);
-    if (isLEFTdepressed)  cam.translateRelativeToView(-CAMERA_STEP, 0);
+    if (isDOWNdepressed) cam.translateRelativeToView(0, CAMERA_STEP);
+    if (isUPdepressed) cam.translateRelativeToView(0, -CAMERA_STEP);
+    if (isLEFTdepressed) cam.translateRelativeToView(-CAMERA_STEP, 0);
     if (isRIGHTdepressed) cam.translateRelativeToView(CAMERA_STEP, 0);
   }
 
@@ -88,12 +110,11 @@ public class CamController
     }
   }
 
+  
   @Override
-  public void keyTyped(KeyEvent e)
-  {
-    super.keyTyped(e);
-  }
+  public void keyTyped(KeyEvent e){ /*do nothing*/}
 
+  
   @Override
   public void keyReleased(KeyEvent e)
   {
@@ -117,59 +138,83 @@ public class CamController
       default:
     }
   }
+  
 
   @Override
   public void mouseClicked(MouseEvent e)
   {
     Point2D mapClick = convertToMapSpace(e.getPoint());
 
-    if(e.isControlDown())
+    if (e.isControlDown())
     {
       cam.centerAbsolute(mapClick.getX(), mapClick.getY());
     }
     else
     {
-      worldPresenter.clickAt(mapClick.getX(), mapClick.getY());
+      presenter.clickAt(mapClick.getX(), mapClick.getY());
     }
   }
 
+  
   @Override
   public void mousePressed(MouseEvent e)
   {
+    isInDrag = true;
 
   }
 
+  
   @Override
   public void mouseReleased(MouseEvent e)
   {
+    draggedFrom = e.getPoint();
+    
 
   }
 
+  
   @Override
   public void mouseEntered(MouseEvent e)
   {
 
   }
 
+  
   @Override
   public void mouseExited(MouseEvent e)
   {
 
   }
 
+  
   @Override
   public void mouseWheelMoved(MouseWheelEvent e)
   {
     Point2D mapClick = convertToMapSpace(e.getPoint());
-    
-    cam.zoomAbsolute(e.getPreciseWheelRotation()/5, mapClick.getX(), mapClick.getY());
+
+    cam.zoomAbsolute(e.getPreciseWheelRotation() / 5, mapClick.getX(), mapClick.getY());
 
   }
+
+  
+  @Override
+  public void mouseDragged(MouseEvent e)
+  {
+    isInDrag = true;
+  }
+
+  
+  @Override
+  public void mouseMoved(MouseEvent e)
+  {
+
+  }
+
   
   private Point2D convertToMapSpace(Point2D screenClick)
   {
     AffineTransform a = cam.getTransform();
-    
+
     try
     {
       a.invert();
