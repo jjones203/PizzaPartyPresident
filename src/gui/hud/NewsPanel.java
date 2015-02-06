20c405f9f53454e181ee1cb17567b4b5177d64fd
David Ringo <davidmringo@gmail.com>
2015-02-05 23:03:10 -0700

    start building ticker panel skeleton

---
+++ src/gui/hud/NewsPanel.java
@@ -0,0 +1,18 @@
+package gui.hud;
+
+import gui.WorldPresenter;
+
+/**
+ @author david
+ created: 2015-02-05
+
+ description: */
+public class NewsPanel
+{
+ 
+ WorldPresenter presenter;
+ 
+ 
+ 
+ 
+}
---
+++ src/gui/hud/Ticker.java
@@ -0,0 +1,18 @@
+package gui.hud;
+
+import gui.WorldPresenter;
+
+import javax.swing.*;
+
+/**
+ @author david
+ created: 2015-02-05
+
+ description: */
+public class Ticker extends JPanel
+{
+ 
+ WorldPresenter presenter;
+
+ 
+}
