fa216e62b9cd01d7b394851fb1d8445910d7c3c4
Winston Riley <masonwr@mac.com>
2015-03-15 17:25:35 -0600

    adds joels new common files to repo

---
+++ src/worldfoodgame/common/AbstractCountryBorderData.java
@@ -0,0 +1,10 @@
+package worldfoodgame.common;
+
+/**
+ * The AbstractCountryBorderData class should be extended by a class that
+ * somehow represents a country's boarder.
+ */
+public abstract class AbstractCountryBorderData
+{
+
+}
