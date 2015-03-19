c8377fb5afd4d00706ac2fc2b286e10778da08b1
Winston Riley <masonwr@mac.com>
2015-03-19 12:43:26 -0600

    patching CSVeditor from a150e2b9d0070d207d91271c5b45dc1a0fd2336f

--- src/worldfoodgame/gui/CSVEditor.java
+++ src/worldfoodgame/gui/CSVEditor.java
@@ -1,51 +1,27 @@
 package worldfoodgame.gui;
 
-import org.apache.commons.csv.*;
-
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import worldfoodgame.IO.CSVParsingException;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-<<<<<<< HEAD
-import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
-import javax.swing.table.TableColumnModel;
-
-import java.awt.BorderLayout;
-import java.awt.Component;
-=======
-import javax.swing.table.DefaultTableModel;
-
->>>>>>> d359edf7f59042e01ea716290634317088262a6a
-import java.awt.Container;
-import java.awt.Dialog;
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.Vector;
 
-<<<<<<< HEAD
 /**
  * Dialog window for correcting errors found when parsing csv file of country data.
  * TO DO: get scrolling to work correctly; highlight cell with error
  * @author jessica
  * @version 18-Mar-2015
  */
-=======
->>>>>>> d359edf7f59042e01ea716290634317088262a6a
 public class CSVEditor extends JDialog implements ActionListener
 {
   private String[] headers;
@@ -53,26 +29,22 @@ public class CSVEditor extends JDialog implements ActionListener
   private CSVParsingException exception;
   private CSVTableModel tableModel;
   private JTable table;
-  
-<<<<<<< HEAD
+
   /**
    * Make and display new CSVEditor
    * @param headers   string array of column names
    * @param records   list of CSVRecords from file
    * @param exception CSVParsingException thrown when parsing
    */
-=======
->>>>>>> d359edf7f59042e01ea716290634317088262a6a
   public CSVEditor(String[] headers, List<CSVRecord> records, CSVParsingException exception)
   {
     this.headers = headers;
     this.records = records;
     this.exception = exception;
-    
+
     setModalityType(Dialog.DEFAULT_MODALITY_TYPE);
     setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-<<<<<<< HEAD
-    
+
     tableModel = makeTableModel(headers, records);
     table = new JTable(tableModel);
     for (int i = 0; i < headers.length; ++i)
@@ -80,59 +52,39 @@ public class CSVEditor extends JDialog implements ActionListener
       TableColumn column = table.getColumnModel().getColumn(i);
       column.setMinWidth(75);
     }
-          
+
     JScrollPane scrollPane = new JScrollPane(table);
     table.setPreferredScrollableViewportSize(new Dimension(900,200));
     table.setFillsViewportHeight(true);
     scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
     scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-=======
-    setMinimumSize(new Dimension(500,500));
-    
-    tableModel = makeTableModel(headers, records);
-    table = new JTable(tableModel);
-    
-    JScrollPane scrollPane = new JScrollPane(table);
-    table.setFillsViewportHeight(true);
->>>>>>> d359edf7f59042e01ea716290634317088262a6a
-    
+
     JButton saveButton = new JButton("SAVE AND CONTINUE");
     saveButton.addActionListener(this);
-    
-<<<<<<< HEAD
+
     // text area standing in for highlighting for time being
     String colName = exception.field;
     long recNum = exception.record.getRecordNumber()-1;
     String errMsg = new String("Error in "+colName+" column of Record "+recNum);
     JTextArea text = new JTextArea(errMsg);
-    
+
     Container contentPane = getContentPane();
     contentPane.add(text,BorderLayout.PAGE_START);
     contentPane.add(scrollPane,BorderLayout.CENTER);
     contentPane.add(saveButton,BorderLayout.PAGE_END);
-    
+
     this.setContentPane(contentPane);
     this.pack();
-=======
-    Container contentPane = getContentPane();
-    contentPane.add(scrollPane);
-    contentPane.add(saveButton);
-    contentPane.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-    this.setContentPane(contentPane);
->>>>>>> d359edf7f59042e01ea716290634317088262a6a
     this.setVisible(true);
-    
+
   }
-  
-<<<<<<< HEAD
+
   /* 
    * actionPerformed is called when user clicks "SAVE AND CONTINUE" button.
    * Saves to csv file associated with this editor's exception member variable.
    * (non-Javadoc)
    * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
    */
-=======
->>>>>>> d359edf7f59042e01ea716290634317088262a6a
   public void actionPerformed(ActionEvent e)
   {
     try
@@ -145,10 +97,6 @@ public class CSVEditor extends JDialog implements ActionListener
       {
         printer.printRecord(nestedVector);
       }
-<<<<<<< HEAD
-=======
-      //printer.print(dataVector);
->>>>>>> d359edf7f59042e01ea716290634317088262a6a
       printer.close();
       writer.close();
       dispose();
@@ -158,8 +106,8 @@ public class CSVEditor extends JDialog implements ActionListener
       exception.printStackTrace();
     }
   }
-  
-  
+
+
   private CSVTableModel makeTableModel(String[] headers, List<CSVRecord> records)
   {
     int cols = headers.length;
@@ -177,45 +125,39 @@ public class CSVEditor extends JDialog implements ActionListener
     CSVTableModel tableModel = new CSVTableModel(tableArray, headers);
     return tableModel;
   }
-  
+
   private void stopEditing()
   {
     if (table.getCellEditor() != null) table.getCellEditor().stopCellEditing();
- }
+  }
 
-  
-<<<<<<< HEAD
+
   /**
    * Class extends DefaultTableModel; use in creating table containing csv data.
    * Prevents user from editing row of table with data types.
    * @author jessica
    * @version 18-Mar-15
    */
-=======
->>>>>>> d359edf7f59042e01ea716290634317088262a6a
   private class CSVTableModel extends DefaultTableModel
   {
     public CSVTableModel(Object[][] data, Object[] columnNames)
     {
       super(data,columnNames);
     }
-    
+
     public boolean isCellEditable(int row, int column)
     {
       // user can't edit row with data types
       if (row != 0) return true;
       else return false;
     }
-<<<<<<< HEAD
-    
+
     public Class<?> getColumnClass(int columnIndex)
     {
       return String.class;
     }
-    
-=======
->>>>>>> d359edf7f59042e01ea716290634317088262a6a
+
   }
-  
-  
+
+
 }
