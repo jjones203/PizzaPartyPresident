20489a930e6e4f95d6479ca844db1a526633b6fb
Winston Riley <masonwr@mac.com>
2015-03-21 13:12:52 -0600

    adds get country method

--- src/worldfoodgame/IO/XMLparsers/CountryXMLparser.java
+++ src/worldfoodgame/IO/XMLparsers/CountryXMLparser.java
@@ -168,6 +168,11 @@ public class CountryXMLparser extends DefaultHandler
     return regionList;
   }
 
+  public Collection<Country> getCountries()
+  {
+    return RegionsToCountries(getRegionList());
+  }
+
   /* private method to generate the set of regions*/
   private void generateRegions()
   throws ParserConfigurationException, SAXException
