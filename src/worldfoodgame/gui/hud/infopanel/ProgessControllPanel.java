85a5b93a7f8174d45896d250e24f7fb1195a847c
Winston Riley <masonwr@mac.com>
2015-03-23 11:37:11 -0600

    gets rough draft of progress panel up and in the repo

--- src/worldfoodgame/gui/hud/infopanel/ProgessControllPanel.java
+++ src/worldfoodgame/gui/hud/infopanel/ProgessControllPanel.java
@@ -11,7 +11,7 @@ import java.awt.event.MouseEvent;
 /**
  * Created by winston on 3/23/15.
  */
-public class ProgessControllPanel extends JFrame
+public class ProgessControllPanel extends JPanel
 {
   private static Color
     DEFAULT_FONT_COL = ColorsAndFonts.GUI_TEXT_COLOR,
@@ -27,24 +27,30 @@ public class ProgessControllPanel extends JFrame
   public ProgessControllPanel(WorldPresenter worldPresenter) throws HeadlessException
   {
     this.worldPresenter = worldPresenter;
-    this.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+    this.setLayout(new GridLayout(1, 6));
 
     JPanel controlls = new JPanel();
     controlls.setLayout(new GridLayout(1,3));
-    this.add(controlls, BorderLayout.WEST);
+    controlls.setBackground(ColorsAndFonts.GUI_BACKGROUND);
+
 
     nextYear = new SingleClickButton("next");
     controlls.add(nextYear);
 
     run = new SingleClickButton("run");
-    controlls.add(nextYear);
+    controlls.add(run);
 
     pause = new SingleClickButton("pause");
     controlls.add(pause);
 
     // todo change this so that i get the world population form the world presenter.
-    yearLabel = getNumbericalLabel("World Population", "15");
+    add(getNumbericalLabel("Current Year", "2015"));
+    add(getNumbericalLabel("Years Remaining", "20"));
+    yearLabel = getNumbericalLabel("World Population", "8 billion");
     add(yearLabel);
+    add(getNumbericalLabel("Happiness %", "45.2"));
+
+    this.add(controlls, BorderLayout.WEST);
   }
 
 
@@ -54,13 +60,17 @@ public class ProgessControllPanel extends JFrame
     jPanel.setLayout(new BorderLayout());
     jPanel.setBackground(ColorsAndFonts.GUI_BACKGROUND);
 
+
     JLabel titleLabel = new JLabel(label);
     titleLabel.setFont(ColorsAndFonts.GUI_FONT.deriveFont(12f));
+    titleLabel.setForeground(ColorsAndFonts.GUI_TEXT_COLOR);
 //    titleLabel.setBackground(ColorsAndFonts.GUI_BACKGROUND);
 
     jPanel.add(titleLabel, BorderLayout.NORTH);
 
     JLabel numbericalValue = new JLabel(formatedNum);
+    numbericalValue.setFont(ColorsAndFonts.GUI_FONT.deriveFont(20f));
+    numbericalValue.setForeground(ColorsAndFonts.GUI_TEXT_COLOR);
     jPanel.add(numbericalValue, BorderLayout.CENTER);
 
     return jPanel;
@@ -76,12 +86,18 @@ public class ProgessControllPanel extends JFrame
     {
       super(text);
       this.label = text;
+      this.setForeground(ColorsAndFonts.GUI_TEXT_COLOR);
+
       addMouseListener(new MouseAdapter()
       {
         @Override
         public void mouseClicked(MouseEvent e)
         {
-          action.run();
+          if (action != null) action.run();
+          else
+          {
+            System.err.println("action not set on Single click button");
+          }
         }
 
         @Override
@@ -106,7 +122,7 @@ public class ProgessControllPanel extends JFrame
     JFrame jFrame = new JFrame();
 
     jFrame.add(pcp);
-    jFrame.pack();
+    jFrame.setSize(900, 60);
     jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     jFrame.setVisible(true);
   }
