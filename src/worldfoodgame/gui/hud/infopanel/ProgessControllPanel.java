ec029b1414c0dad460134a746cac16d45985d240
Winston Riley <masonwr@mac.com>
2015-03-23 10:57:29 -0600

    fleshes out Progess Controll panel a bit more

--- src/worldfoodgame/gui/hud/infopanel/ProgessControllPanel.java
+++ src/worldfoodgame/gui/hud/infopanel/ProgessControllPanel.java
@@ -1,11 +1,79 @@
 package worldfoodgame.gui.hud.infopanel;
 
+import worldfoodgame.gui.ColorsAndFonts;
+import worldfoodgame.gui.WorldPresenter;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * Created by winston on 3/23/15.
  */
 public class ProgessControllPanel extends JFrame
 {
+  private static Color
+    DEFAULT_FONT_COL = ColorsAndFonts.GUI_TEXT_COLOR,
+    ROLLOVER_COLOR = Color.red,
+    SELECTED_COL = Color.yellow;
 
+  private WorldPresenter worldPresenter;
+  private SingleClickButton
+    nextYear, run, pause;
+
+  public ProgessControllPanel(WorldPresenter worldPresenter) throws HeadlessException
+  {
+    this.worldPresenter = worldPresenter;
+    this.setLayout(new BorderLayout());
+
+    JPanel controlls = new JPanel();
+    this.add(controlls, BorderLayout.EAST);
+
+    nextYear = new SingleClickButton("next year");
+    controlls.add(nextYear);
+
+    run = new SingleClickButton("run");
+    controlls.add(nextYear);
+
+    pause = new SingleClickButton("pause");
+    controlls.add(pause);
+
+
+  }
+
+  private class SingleClickButton extends JLabel
+  {
+    private String label;
+    private Runnable action;
+
+    public SingleClickButton(String text)
+    {
+      super(text);
+      this.label = text;
+      addMouseListener(new MouseAdapter()
+      {
+        @Override
+        public void mouseClicked(MouseEvent e)
+        {
+          action.run();
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e)
+        {
+          SingleClickButton.this.setForeground(ROLLOVER_COLOR);
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e)
+        {
+          SingleClickButton.this.setForeground(DEFAULT_FONT_COL);
+        }
+      });
+    }
+
+
+
+  }
 }
