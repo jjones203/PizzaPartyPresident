17d16c618eba7c0e6545721fe0b897bcdde32975
Winston Riley <masonwr@mac.com>
2015-03-25 15:43:23 -0600

    adds flag loader class to repo

---
+++ src/worldfoodgame/gui/FlagLoader.java
@@ -0,0 +1,27 @@
+package worldfoodgame.gui;
+
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+
+/**
+ * Created by winston on 3/25/15.
+ */
+public class FlagLoader
+{
+  private static FlagLoader flagLoader;
+  private HashMap<String, BufferedImage> images;
+
+  /**
+   * Returns the countries associated flag as a biffered image
+   * @param countryName
+   * @return
+   */
+  public static BufferedImage getFlag(String countryName)
+  {
+
+  }
+
+  private FlagLoader()
+  {
+  }
+}
