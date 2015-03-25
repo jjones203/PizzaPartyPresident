3e776f1ce21be2a8471d4aee5c7e8b88f6a2fc75
Winston Riley <masonwr@mac.com>
2015-03-25 16:56:01 -0600

    bit of clean up

--- src/worldfoodgame/gui/regionlooks/RegionNameDraw.java
+++ src/worldfoodgame/gui/regionlooks/RegionNameDraw.java
@@ -13,18 +13,19 @@ import java.awt.image.BufferedImage;
  * Created by winston on 1/27/15.
  * Phase_01
  * CS 351 spring 2015
- * 
+ * <p/>
  * RegionView that draws the names of Regions somewhere near the center of the
  * region via a static method, which is called in an order or with an appropriate
- * graphics context to allow the names to be seen above other display elements 
+ * graphics context to allow the names to be seen above other display elements
  */
 public class RegionNameDraw
 {
 
   /**
-   Draw the name of a GUIRegion on a given graphics context
-   @param g   graphics context to draw to
-   @param gRegion   GUIRegion whose name is to be drawn
+   * Draw the name of a GUIRegion on a given graphics context
+   *
+   * @param g       graphics context to draw to
+   * @param gRegion GUIRegion whose name is to be drawn
    */
   public static void draw(Graphics g, GUIRegion gRegion)
   {
@@ -46,17 +47,18 @@ public class RegionNameDraw
     /* find ~center of region in map-space */
     int x = (int) gRegion.getPoly().getBounds().getCenterX();
     int y = (int) gRegion.getPoly().getBounds().getCenterY();
-    
-    
-    Point src = new Point(x,y);
+
+
+    Point src = new Point(x, y);
     Point dst = new Point();
-    
-    try{
+
+    try
+    {
       /* convert center point to screen space for drawing */
       AffineTransform atINV = new AffineTransform(at);
       atINV.invert();
       at.transform(src, dst);
-    } 
+    }
     catch (NoninvertibleTransformException e)
     { /* should never happen: all map transforms should be invertible */
       e.printStackTrace();
@@ -69,8 +71,16 @@ public class RegionNameDraw
       .getFlag(gRegion.getRegion().getCountry().getName());
 
 
-    g2d.drawImage(flag, dst.x - flag.getWidth(), dst.y - flag.getHeight(), null);
+    if (flag != null)
+    {
+      // magic numbers are padding to position the flag
+      int imgX = dst.x - flag.getWidth() - 3;
+      int imgY = dst.y - flag.getHeight() + 3;
+      g2d.drawImage(flag, imgX, imgY, null);
+    }
+
+
     /* reset the transform for any RegionViews depending on it for proper rendering */
-      g2d.setTransform(at);
+    g2d.setTransform(at);
   }
 }
