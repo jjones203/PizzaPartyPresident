c0c236fb7d2c65b05b3226505161e0b92df1588e
JESSICA JONES <JJONES203@UNM.EDU>
2015-03-26 12:17:02 -0600

    rating tiles' suitability for each crop + related changes

--- src/worldfoodgame/model/CropClimateData.java
+++ src/worldfoodgame/model/CropClimateData.java
@@ -11,28 +11,28 @@ public interface CropClimateData
   int RICE_MIN_TEMP = 10;
   int RICE_DAY_TEMP = 35;
   int RICE_NIGHT_TEMP = 20;
-  int RICE_MIN_RAIN = 70;
   int RICE_MAX_RAIN = 400;
+  int RICE_MIN_RAIN = 70;
   
   int WHEAT_MAX_TEMP = 45; 
   int WHEAT_MIN_TEMP = -20;
   int WHEAT_DAY_TEMP = 20;
   int WHEAT_NIGHT_TEMP = 10;
-  int WHEAT_MIN_RAIN = 25;
   int WHEAT_MAX_RAIN = 78;
+  int WHEAT_MIN_RAIN = 25;
   
   int CORN_MAX_TEMP = 45; 
   int CORN_MIN_TEMP = 0;
   int CORN_DAY_TEMP = 29;
   int CORN_NIGHT_TEMP = 20;
-  int CORN_MIN_RAIN = 60;
   int CORN_MAX_RAIN = 100;
+  int CORN_MIN_RAIN = 60;
   
   int SOY_MAX_TEMP = 45; 
   int SOY_MIN_TEMP = 7;
   int SOY_DAY_TEMP = 35;
   int SOY_NIGHT_TEMP = 30;
-  int SOY_MIN_RAIN = 45;
   int SOY_MAX_RAIN = 70;
+  int SOY_MIN_RAIN = 45;
   
 }
