60b3ff12c8748f9008963a2ffbe3b972a4a17efe
Winston Riley <masonwr@mac.com>
2015-04-04 15:04:12 -0600

    adds max temp view

---
+++ src/worldfoodgame/gui/regionlooks/MaxTemp.java
@@ -0,0 +1,15 @@
+package worldfoodgame.gui.regionlooks;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by winston on 4/4/15.
+ */
+public class MaxTemp extends RasterViz
+{
+  @Override
+  public BufferedImage getRasterImage()
+  {
+    return null;
+  }
+}
