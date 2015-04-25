31b5d9e12969ed4a7807ba8b7a78aead0873d554
ststromberg <ststromb@gmail.com>
2015-04-25 15:26:19 -0600

    Basic Planning Points Work
    Package, Panel, and Interface basics

---
+++ src/worldfoodgame/planningpoints/PlanningPointsAllocationPanel.java
@@ -0,0 +1,34 @@
+package worldfoodgame.planningpoints;
+
+import java.util.Collection;
+
+import worldfoodgame.model.Region;
+
+/**
+ * 
+ * @author Stephen Stromberg on 4/25/15
+ *
+ *Represents the main gui interface that the player will
+ *have for allocating planning points at the end of each
+ *harvest/year.
+ */
+public class PlanningPointsAllocationPanel
+{
+  private PlanningPointsInteractableRegion playerRegion;
+  private Collection<PlanningPointsInteractableRegion> otherRegions;
+  private int playerPlanningPoints;
+  
+  public PlanningPointsAllocationPanel
+  (
+      PlanningPointsInteractableRegion playerRegion,
+      Collection<PlanningPointsInteractableRegion> otherRegions,
+      int playerPlanningPoints
+  )
+  {
+    this.playerRegion = playerRegion;
+    this.otherRegions = otherRegions;
+    this.playerPlanningPoints = playerPlanningPoints;
+  }
+  
+
+}
---
+++ src/worldfoodgame/planningpoints/PlanningPointsInteractableRegion.java
@@ -0,0 +1,17 @@
+package worldfoodgame.planningpoints;
+
+/**
+ * 
+ * @author Stephen Stromberg on 4/25/12
+ * 
+ * Represents a region that has all methods 
+ * necessary for the MS3 implementation
+ * of planning points. These methods are
+ * to be called from an instance of the
+ * PlanningPointsAllocationPanel.
+ *
+ */
+public interface PlanningPointsInteractableRegion
+{
+
+}
