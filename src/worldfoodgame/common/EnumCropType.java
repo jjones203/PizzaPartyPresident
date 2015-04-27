57f8a8d01eb93006510581941cddfdb9bf06e6b4
Ken Kressin <jemezgeek.gmail.com>
2015-04-27 15:53:17 -0600

    Added a getOldName method to convert to old crop names.

--- src/worldfoodgame/common/EnumCropType.java
+++ src/worldfoodgame/common/EnumCropType.java
@@ -4,15 +4,15 @@ import worldfoodgame.model.CropClimateData;
 
 public enum EnumCropType implements CropClimateData
 {
-  WHEAT("Wheat", WHEAT_MAX_TEMP, WHEAT_MIN_TEMP, WHEAT_DAY_TEMP, WHEAT_NIGHT_TEMP, WHEAT_MAX_RAIN, WHEAT_MIN_RAIN, WHEAT_WATER_USE),
+  WHEAT("tomatoes", WHEAT_MAX_TEMP, WHEAT_MIN_TEMP, WHEAT_DAY_TEMP, WHEAT_NIGHT_TEMP, WHEAT_MAX_RAIN, WHEAT_MIN_RAIN, WHEAT_WATER_USE),
   
-  RICE("Rice", RICE_MAX_TEMP, RICE_MIN_TEMP, RICE_DAY_TEMP, RICE_NIGHT_TEMP, RICE_MAX_RAIN, RICE_MIN_RAIN, RICE_WATER_USE),
+  RICE("pineapple", RICE_MAX_TEMP, RICE_MIN_TEMP, RICE_DAY_TEMP, RICE_NIGHT_TEMP, RICE_MAX_RAIN, RICE_MIN_RAIN, RICE_WATER_USE),
   
-  CORN("Corn", CORN_MAX_TEMP, CORN_MIN_TEMP, CORN_DAY_TEMP, CORN_NIGHT_TEMP, CORN_MAX_RAIN, CORN_MIN_RAIN, CORN_WATER_USE),
+  CORN("peppers", CORN_MAX_TEMP, CORN_MIN_TEMP, CORN_DAY_TEMP, CORN_NIGHT_TEMP, CORN_MAX_RAIN, CORN_MIN_RAIN, CORN_WATER_USE),
   
-  SOY("Soy", SOY_MAX_TEMP, SOY_MIN_TEMP, SOY_DAY_TEMP, SOY_NIGHT_TEMP, SOY_MAX_RAIN, SOY_MIN_RAIN, SOY_WATER_USE),
+  SOY("'shrooms", SOY_MAX_TEMP, SOY_MIN_TEMP, SOY_DAY_TEMP, SOY_NIGHT_TEMP, SOY_MAX_RAIN, SOY_MIN_RAIN, SOY_WATER_USE),
   
-  OTHER_CROPS("Other"); // use constructor that sets fields to null because will vary by country
+  OTHER_CROPS("pepperoni"); // use constructor that sets fields to null because will vary by country
   
   public final String name;
   public final Integer maxTemp; // use Integer because need to be able to assign null
